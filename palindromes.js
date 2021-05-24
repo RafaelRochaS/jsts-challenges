f6a8ef7c286527eb3c890c967be509fb48edb83a
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-05-24 20:34:59 -0300

    Palindromes finished

--- palindromes.js
+++ palindromes.js
@@ -1,8 +1,38 @@
+// From: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
+
 function palindrome(str) {
 
-  return false;
+  // Short, fast version
+  return str.replace(/[^0-9a-z]/gi, '').toLowerCase().split('').reverse().join('') === str.replace(/[^0-9a-z]/gi, '').toLowerCase();
 }
 
+function palindromeManual(str) {
+
+  // Slow, manual version
+
+  str = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+
+  strArr = [];
+  reverseArr = [];
+
+  for (let c of str) {
+    strArr.push(c);
+  }
+
+  for (let index = strArr.length - 1; index >= 0; index--) {
+    reverseArr.push(strArr[index]);
+  }
+
+  for (let index = 0; index < strArr.length; index++) {
+    if (reverseArr[index] !== strArr[index]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Freecodecamp tests
 console.log(palindrome("eye"));
 console.log(palindrome("_eye"));
 console.log(palindrome("race car"));
@@ -15,3 +45,38 @@ console.log(palindrome("My age is 0, 0 si ega ym."));
 console.log(palindrome("1 eye for of 1 eye."));
 console.log(palindrome("0_0 (: /-\ :) 0-0"));
 console.log(palindrome("five|\_/|four"));
+
+// Performance tests
+console.log('\n[*] Timing One liner...');
+console.time('One liner');
+palindrome("eye");
+palindrome("_eye");
+palindrome("race car");
+palindrome("not a palindrome");
+palindrome("A man, a plan, a canal. Panama");
+palindrome("never odd or even");
+palindrome("nope");
+palindrome("almostomla");
+palindrome("My age is 0, 0 si ega ym.");
+palindrome("1 eye for of 1 eye.");
+palindrome("0_0 (: /-\ :) 0-0");
+palindrome("five|\_/|four");
+console.timeEnd('One liner');
+console.log('[+] One liner done');
+
+console.log('\n\n[*] Timing manual...');
+console.time('Manual');
+palindromeManual("eye");
+palindromeManual("_eye");
+palindromeManual("race car");
+palindromeManual("not a palindrome");
+palindromeManual("A man, a plan, a canal. Panama");
+palindromeManual("never odd or even");
+palindromeManual("nope");
+palindromeManual("almostomla");
+palindromeManual("My age is 0, 0 si ega ym.");
+palindromeManual("1 eye for of 1 eye.");
+palindromeManual("0_0 (: /-\ :) 0-0");
+palindromeManual("five|\_/|four");
+console.timeEnd('Manual');
+console.log('[+] Manual done');
